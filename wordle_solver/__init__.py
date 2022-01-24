fbe349038ced863b283e83b858ff1b7caca507ca
Riley Chase <riley.chase@rileychase.net>
2022-01-24 20:16:11 +1030

    Setup package __init__.py

---
+++ wordle_solver/__init__.py
@@ -0,0 +1,7 @@
+from .constants import CORRECT, IN_WORD, INCORRECT, Answer, AnswerPart
+from .renderer import RichWordleRenderer
+from .solver import WordleSolver
+from .words import ALL_WORDS
+
+__program__ = "wordle_solver"
+__version__ = "1.1.0"
