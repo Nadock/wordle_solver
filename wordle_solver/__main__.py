35574770d50c66e9fac3509c19cf35f5e88dd17c
Riley Chase <riley.chase@rileychase.net>
2022-01-24 20:16:19 +1030

    Setup package __main__.py

---
+++ wordle_solver/__main__.py
@@ -0,0 +1,22 @@
+import sys
+
+from . import cli, renderer
+
+
+def main():
+    """Parse CLI args and run wordle_solver"""
+    args = cli.parse_cli_args()
+    render = renderer.RichWordleRenderer(args)
+    if args.remain:
+        render.remaining()
+    elif args.suggest:
+        render.suggest()
+    else:
+        render.solve()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(1)
